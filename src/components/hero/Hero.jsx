f89909791b636db111e11703c9deafce079574a2
sava <savamedvedev@gmail.com>
2022-05-06 05:24:25 +0800

    second-commit

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HeroStyles.css';
+
+
+const Hero = () => {
+  return (
+	<div>Hero</div>
+  )
+}
+
+export default Hero
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomeStyles.css";
+import Navbar from "../navbar/Navbar";
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default Home;
