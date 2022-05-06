4ca39049c5f4da62ded79d048e4f82718eb08218
sava <savamedvedev@gmail.com>
2022-05-06 18:20:02 +0800

    four-commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./HomeStyles.css";
 import Navbar from "../navbar/Navbar";
 import Hero from "../hero/Hero";
+import Pricing from "../pricing/Pricing";
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Pricing />
     </>
   );
 };
