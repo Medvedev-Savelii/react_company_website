b07fa3f3a111b8205eb19eec6547ef4f651be5d0
sava <savamedvedev@gmail.com>
2022-05-06 18:38:17 +0800

    five-commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,12 +3,14 @@ import "./HomeStyles.css";
 import Navbar from "../navbar/Navbar";
 import Hero from "../hero/Hero";
 import Pricing from "../pricing/Pricing";
+import Testimonials from "../testimonials/Testimonials";
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
       <Pricing />
+      <Testimonials />
     </>
   );
 };
