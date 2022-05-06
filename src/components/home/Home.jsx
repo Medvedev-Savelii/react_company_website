6bd2a9393b83dcb6f41e6f24dd0855b792ef4b0d
sava <savamedvedev@gmail.com>
2022-05-06 17:36:24 +0800

    therd-commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./HomeStyles.css";
 import Navbar from "../navbar/Navbar";
+import Hero from "../hero/Hero";
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 };
